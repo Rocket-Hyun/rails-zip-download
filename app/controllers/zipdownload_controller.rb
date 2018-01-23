0501f061025185a04a238fb2a7e9f10df4a4cdf7
Junghyun Park <rockethyun@gmail.com>
2018-01-23 11:36:05 +0900

    Update zipdownload_controller.rb

--- app/controllers/zipdownload_controller.rb
+++ app/controllers/zipdownload_controller.rb
@@ -1,9 +1,11 @@
 class ZipdownloadController < ApplicationController
   def index
-    compressed_filestream = Zip::OutputStream.write_buffer(::StringIO.new(''), Zip::TraditionalEncrypter.new('password')) do |zos|
+    compressed_filestream = Zip::OutputStream.write_buffer(::StringIO.new('')) do |zos|
+      folder = "#{Rails.root}/public/data"
       file_list = Dir.entries("public/data/").select {|f| !File.directory? f}
       file_list.each do |text|
-        zos.put_next_entry text
+        zos.put_next_entry(text)
+        zos.print IO.read(File.join(folder, "#{item}.mp3"))
       end
     end
     compressed_filestream.rewind
